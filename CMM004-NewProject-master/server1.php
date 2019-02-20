67144a8f175783bf44db23340c4226fe1fc2cab4
1808234 <1808234@rgu.ac.uk>
2019-02-20 17:17:39 +0000

    all good done i think :)

--- CMM004-NewProject-master/server1.php
+++ CMM004-NewProject-master/server1.php
@@ -8,7 +8,7 @@ $errors= array();
  $db = mysqli_connect('CSDM-WEBDEV','1809441','1809441','db1809441_trade');
 /*if($db-> connect_error) {die('Error'.('.$db->connect_errno.'));} else {echo "connected";}
 */
-
+// user login check :)
 if (isset($_POST['login'])) {
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
@@ -28,60 +28,28 @@ if (isset($_POST['login'])) {
        $password = md5($password);
         $query = "SELECT * FROM user_info WHERE email='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
-        echo "test_2";
-        if (mysqli_num_rows($results) == 1) {
-            echo "test_3";
+        $query_tr = "SELECT * FROM tradesman_info WHERE email='$username' AND password='$password'";
+        $results_2 = mysqli_query($db, $query_tr);
+
+        if (mysqli_num_rows($results) == 1 OR mysqli_num_rows($results_2) == 1) {
+
             $_SESSION['username'] = $username;
             $_SESSION['success'] = "You are now logged in";
             header('location: index.php/?weDoneIt');
-            echo "test_4";
+
         }else {
-            echo "test_5";
+
             array_push($errors, "Wrong username/password combination");
-            header('location: index.php/?wrong');
-            echo "test_6";}
+            header('location: index.php/?wrong_1');
+            }
     }
-}
-
-
-
-
-
-
-
-
-
- /*
- if(isset($_POST['login'])) {
-     $lo_username = filter_input($db, $_POST['username5']);
-     $lo_password = filter_input($db, $_POST['password5']);
-
-     if (empty($lo_username)) {
-         array_push($error, "username is required");
-     }
-     if (empty($lo_password)) {
-         array_push($error, "password is required");
-     }
-
-     if (count($error) == 0) {
-         $do = "SELECT * FROM user_info WHERE email='" . $lo_username . "' OR password= '" . $lo_password . "'";
-         $result = mysqli_query($db, $do);
-         if (mysqli_num_rows($result) == 1) {
-             //$_SESSION ['username']= $lo_username;
-             //  $_SESSION['success']= "you are no logged in Successfully";
-             echo " we done it 2";
-         }
-
-     }
- }
-
-
-
-
-
+} // end of the user login checks
 
 /**/
+
+
 //register user
+
 if(isset($_POST['userRej'])){
 
     $username= mysqli_real_escape_string($db,$_POST['username']);
@@ -131,13 +99,73 @@ if(isset($_POST['userRej'])){
         mysqli_query($db, $query);
         $_SESSION['username'] = $username;
         $_SESSION['success'] = "You are now logged in";
-        header('Location: /server1.php?allGood');
+        header('Location: /server2.php?allGood_youAre_in');
+    }
+} // end of the user registration form and starting of the trades form
+
+/**/
+
+// starting the tradesman registration form  ;)
+
+if(isset($_POST['tradesRej'])){
+
+    $username= mysqli_real_escape_string($db,$_POST['username_tr']);
+    $email = mysqli_real_escape_string($db,$_POST['email_tr']);
+    $password_1= mysqli_real_escape_string($db,$_POST['password_tr']);
+    $password_2 = mysqli_real_escape_string($db,$_POST['password1_tr']);
+    $f_name= mysqli_real_escape_string($db,$_POST['1name_tr']);
+    $L_name= mysqli_real_escape_string($db,$_POST['u2name_tr']);
+    $add_1= mysqli_real_escape_string($db,$_POST['add1_tr']);
+    $add_2= mysqli_real_escape_string($db,$_POST['add2_tr']);
+    $city= mysqli_real_escape_string($db,$_POST['city_tr']);
+    $postcode= mysqli_real_escape_string($db,$_POST['postcode_tr']);
+    $profession= mysqli_real_escape_string($db,$_POST['profession']);
+    $ab_you= mysqli_real_escape_string($db,$_POST['AboutYou']);
+    $qua= mysqli_real_escape_string($db,$_POST['qua']);
+
+    //to make sure all or most of the places are filled correctly
+    if(empty($username)){ array_push($error,"username is required");}
+    if(empty($email)){ array_push($error,"email is required");}
+    if(empty($password_1)){ array_push($error,"Password is required");}
+    if($password_1 != $password_2){array_push($error,"the password do not match");}
+    if(empty($f_name)){ array_push($error,"First name is required");}
+    if(empty($L_name)){ array_push($error,"Last name is required");}
+    if(empty($add_1)){ array_push($error,"the first line is required");}
+    if(empty($city)){ array_push($error,"city name is required");}
+    if(empty($postcode)){ array_push($error,"postcode is required");}
+
+    // first check the database to make sure
+    // a user does not already exist with the same username and/or email
+    $user_check_query1 = "SELECT * FROM user_info WHERE username='$username' OR email='$email' LIMIT 1";
+    $result1 = mysqli_query($db, $user_check_query1);
+    $trad = mysqli_fetch_assoc($result1);
+
+    if ($trad) { // if user exists
+        if ($trad['username'] === $username) {
+            array_push($errors, "Username already exists");
+        }
+
+        if ($trad['email'] === $email) {
+            array_push($errors, "email already exists");
+        }
     }
 
+    // then, register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = md5($password_1);//encrypt the password before saving in the database
 
+        $query2 = "INSERT INTO tradesman_info (firstname,lastname,add1,add2,city,postcode,username, email, password,about_you,qualifications) 
+  			  VALUES('$f_name','$L_name','$add_1','$add_2','$city','$postcode','$username', '$email', '$password','$ab_you','$qua')";
+        mysqli_query($db, $query2);
+        $pro= "INSERT INTO trades_prof (profession) VALUES ('$profession')";
+        mysqli_query($db,$pro);
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+        header('Location: /server1.php?allGood_youAre_in');
+    }
+}// end of the tradesman registration form
 
+// all done for now see you soon
 
-
-}
 
 
